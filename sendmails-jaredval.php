ade46b80213d11a07d6b5bca417c84bf32dfde46
Cerulean-1xk <127709418+Cerulean-1xk@users.noreply.github.com>
2023-03-16 12:43:35 -0500

    Update sendmails-jaredval.php

--- sendmails-jaredval.php
+++ sendmails-jaredval.php
@@ -23,6 +23,8 @@ if (isset($_REQUEST['silent']) && $_REQUEST['silent'] == "1") {
 
 include_once 'common.php';
 require_once 'class.phpmailer.php';
+// New PHPMailer version uses different directory:
+// require_once 'src/PHPMailer.php';
 
 if ($silent != TRUE) {
     print "<html>\n";
